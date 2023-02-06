0d158bd0f4353f4cff879ac13a3b147f82977a83
Matt Soucoup <masoucou@microsoft.com>
2023-02-06 15:19:56 -0800

    Adding the azure function

---
+++ 7-functions/MunsonPickles.Shared/Transfer/NewReview.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MunsonPickles.Shared.Transfer;
+
+public class NewReview
+{
+    public string ReviewText { get; set; } = string.Empty;
+    public int ProductId { get; set; } = 0;
+    public List<string> PhotoUrls { get; set; }= new List<string>();
+}
